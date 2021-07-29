b28e7f3bced6c9aa962ed95dc3df88625ef2f341
rini perencsik <riniperencsik@rinis-MacBook-Pro.local>
2021-07-28 17:07:41 -0700

    updated models

--- wlugo_app/models.py
+++ wlugo_app/models.py
@@ -19,32 +19,32 @@ class Rating(models.Model):
 
 class User(models.Model):
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    email = 
-    password = 
-    first_name = 
-    last_name = 
-    program = 
-    graduating_year = 
-    has_access = 
-    is_mod = 
-    is_admin = 
+    email = models.CharField(max_length=20)
+    password =  models.CharField(max_length=20)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    program = models.CharField(max_length=20)
+    graduating_year = models.IntegerField(max_length=4)
+    has_access = models.BooleanField()
+    is_mod = models.BooleanField()
+    is_admin = models.BooleanField()
 
 class Review(models.Model):
     review_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user_id = 
-    course_id = 
-    body = 
-    rating_id = 
-    grade = 
-    professor_id = 
-    term_taken = 
-    date_time_created = 
+    user_id = models.ForeignKey(User, on_delete = models.CSCADE) 
+    course_id = models.ForeignKey(Course, on_delete = models.CSCADE) 
+    body = models.TextField()
+    rating_id = models.ForeignKey(Rating, on_delete = models.CSCADE) 
+    grade = models.IntegerField(max_length = 2)
+    professor_id = models.ForeignKey(Professor, on_delete = models.CSCADE) 
+    term_taken = models.CharField(max_length=20)
+    date_time_created = models.DateTimeField()
 
 class Professor(models.Model):
     professor_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    first_name = 
-    last_name = 
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
 
-class Professor_course(models.Model): # ?
-    professor = 
-    course_id =
+class Professor_course(models.Model): 
+    professor_id = models.ForeignKey(Professor, on_delete = models.CSCADE) 
+    course_id =models.ForeignKey(Course, on_delete = models.CSCADE) 
